32c9c3496eae5307e3879208436d87e2439ffbae
Pere Isenah <pereisenah@gmail.com>
2023-08-14 17:52:07 +0100

    update 0x02-minimum_operations/0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+def minOperations(n):
+    if n <= 0:
+        return 0
+    
+    operations = 0
+    copy = 1  # The current number of characters in the clipboard
+    chars = 1  # The current number of characters in the text file
+    
+    while chars < n:
+        if n % chars == 0:  # If n is divisible by the current number of characters
+            copy = chars
+        chars += copy
+        operations += 1
+    
+    return operations
+    
